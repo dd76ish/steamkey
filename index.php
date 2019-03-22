c339b7a9c0a620886643e6fc2cabc610c9a3115b
dd76ish <48843267+dd76ish@users.noreply.github.com>
2019-03-23 00:38:38 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+﻿<html lang="en-US" hola_ext_inject="ready"><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>YouSite.ru - Бесплатная раздача ключей STEAM</title> <!-- ИМЯ САЙТА -->
+
+        <link rel="shortcut icon" href="http://gj.by/templates/steam-2/img/favicon.ico">
+        <link rel="stylesheet" href="http://gj.by/templates/steam-2/css/main.css">
+
+	
+<style>
+a{
+ color: white;
+}
+ body {
+    background: rgba(0, 0, 0, 0.72) url(http://hotwalls.ru/wallpapers/igra_star_wars_battlefront_2016/download/3840x2160.jpg); <!-- ССЫЛКА НА ФОН САЙТА-->
+	
+}
+	</style>
+</head>
+<body class="v_image" hola-ext-player="1" id="index-page">
+            <h1 id="logo" style="margin-left: 120px;"><a href="#"><img src="http://gj.by/templates/steam/images/steam-logo.png" alt="Раздача ключей STEAM"></a></h1>
+ <div style="margin-left: 120px;">
+            <section id="home">
+                <header>
+               
+                    <h2>Раздача началась! <span>Ты готов?</span></h2>
+                    <h3>Получи бесплатно ключ к платной игре в&nbsp;&nbsp;STEAM&nbsp;!</h3>
+                </header>
+                <div class="home-section-content">          
+                                <strong>Бесплатные ключи каждую секунду!
+								
+								
+								 <div id='placeForText'></div>
+   
+    
+    
+          <div id='placeForText'></div>
+								
+								</strong>
+                                
+
+                </div>
+            </section>
+
+            <div id="countdown" class="">
+
+				<br><br><br>
+			                <div class="count-el"><span class="deci215947724 num-counter-1 yocto148635873">B0AF0-0W0●●-●●●●●
+</span> 
+					<span class="name-counter-1">
+					
+					<h3>
+					<script type="text/javascript">
+window.onload = function () {
+    var i = 35; <!-- ВРЕМЯ ОЖИДАНИЯ В ОЧЕРЕДИ-->
+    var timer = setInterval(function () {
+        i--;
+        document.getElementById('timer').innerHTML = 'Ты в очереди ' + i;
+        if (i == 0) {
+            document.getElementById('example').style.display = 'block';
+            document.getElementById('timer').style.display = 'none';
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+</script>
+<div id="timer"></div>
+<div id="example" style=" display: none; ">
+<a href="http://dfile.su/d/mfQOgH0i12">Остальная часть ключа!</a>  
+</div>
+			</h3>		
+					
+
+
+					
+					</span></div>
+
+				</div>
+
+
+
+</body></html>
